eb7d9dc708016f8bfd2fd6f60a37a48e22f6d133
Neil Mather <ngmather@gmail.com>
2012-11-24 14:40:33 +0000

    Pull db name from local_settings to avoid cross-platform conflicts.

--- tweetbribe/tweetbribe/settings.py
+++ tweetbribe/tweetbribe/settings.py
@@ -1,5 +1,7 @@
 # Django settings for tweetbribe project.
 
+from tweetbribe.local_settings import DATABASE_NAME
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -12,7 +14,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/Users/markdessain/Documents/tweetbribe/database.sqlite3',                      # Or path to database file if using sqlite3.
+        'NAME': DATABASE_NAME,
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
