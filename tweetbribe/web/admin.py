da04c171d45a94e6fc52d33b897d002e324e8ec6
Mark Dessain <mark.dessain@gmail.com>
2012-11-24 14:22:08 +0000

    Setup some models and admin

---
+++ tweetbribe/web/admin.py
@@ -0,0 +1,5 @@
+from web.models import User, Bribe
+from django.contrib import admin
+
+admin.site.register(User)
+admin.site.register(Bribe)
--- tweetbribe/web/models.py
+++ tweetbribe/web/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    twitter_handle = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+
+class Bribe(models.Model):
+    message = models.CharField(max_length=200)
+    briber = models.ForeignKey(User, related_name='briber')
+    bribee = models.ForeignKey(User, related_name='bribee')
