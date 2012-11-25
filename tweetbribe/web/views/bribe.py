4f382982d18ead4a3de4f0ee0def62b2182eca9f
Neil Mather <ngmather@gmail.com>
2012-11-25 13:34:08 +0000

    fix logos

--- tweetbribe/web/views/bribe.py
+++ tweetbribe/web/views/bribe.py
@@ -14,7 +14,7 @@ def setup(request):
     Charity.objects.all().delete()
 
     charities = []
-    charities.append(Charity(name='British Heart Foundation', description='', givinglab_id='3eb39186-e8a6-4495-ba38-588409080124', logo_url=''))
+    charities.append(Charity(name='British Heart Foundation', description='', givinglab_id='3eb39186-e8a6-4495-ba38-588409080124', logo_url='bhf_logo.jpg'))
     charities.append(Charity(name='Stroke Association', description='', givinglab_id='833422b4-f47d-4511-b5fb-eacb33c59c16', logo_url='/static/logos/strokeassoc.jpg'))
     charities.append(Charity(name='Academy Fm Folkestone', description='', givinglab_id='479a5f6c-3885-48be-ad94-007998942d82', logo_url='/static/logos/academy_fm_folkestone.jpg'))
     charities.append(Charity(name='Multiple Sclerosis Society', description='', givinglab_id='149a4a4a-7c0d-4ac1-93f4-21038bdf8398', logo_url='/static/logos/mssoc.jpeg'))
@@ -24,7 +24,7 @@ def setup(request):
     charities.append(Charity(name='Action For Children', description='', givinglab_id='3e4e9efa-951a-4fa0-8390-47ec999dd8ca', logo_url='/static/logos/action_for_children.jpg'))
     charities.append(Charity(name='Tree of Hope', description='', givinglab_id='18fb1d04-f657-4792-99df-2fc194565be3', logo_url='/static/logos/tree_of_hope.jpg'))
     charities.append(Charity(name='Variety Club', description='', givinglab_id='ff6ccd27-c61a-48e5-9b24-4fbb9ca0ac94', logo_url='/static/logos/variety_club.jpg'))
-    charities.append(Charity(name='Global Children Development', description='', givinglab_id='2d2e32e6-a1f7-49ce-9287-125f16a7638a', logo_url='/static/logos/variety_club.jpg'))
+    charities.append(Charity(name='Global Children Development', description='', givinglab_id='2d2e32e6-a1f7-49ce-9287-125f16a7638a', logo_url='/static/logos/global_childrens_development.jpg'))
     
     for charity in charities:
         charity.save()
